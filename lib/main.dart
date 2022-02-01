4907be52c349b577490e275007fe789b0bd998b8
bhagirathsinh1 <70099455+bhagirathsinh1@users.noreply.github.com>
2022-02-01 11:30:45 +0530

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter AR',
+      title: 'ARDemo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
